b42e3233536e0fae20c475f199ce5a1f1cf29686
root <root@raspberrypi.(none)>
2014-05-09 12:40:29 +0000

    update

---
+++ daemon-test.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+echo ""
+echo "sudo ps -ef | grep emonhub"
+sudo ps -ef | grep emonhub
+echo ""
+echo "sudo service emonhub"
+sudo service emonhub
+echo ""
+echo "sudo service emonhub status"
+sudo service emonhub status
+echo ""
+echo "sudo service emonhub stop"
+sudo service emonhub stop
+echo ""
+echo "sudo service emonhub start"
+sudo service emonhub start
+echo ""
+echo "sudo service emonhub restart"
+sudo service emonhub restart
+echo ""
+echo "sudo service emonhub stop"
+sudo service emonhub stop
+echo ""
+echo "sudo service emonhub restart"
+sudo service emonhub restart
+echo ""
+echo "sudo service emonhub stop"
+sudo service emonhub stop
+echo ""
+echo "sudo service emonhub force-reload"
+sudo service emonhub force-reload
+echo ""
+echo "sudo service emonhub status"
+sudo service emonhub status
+echo ""
+echo "sudo ps -ef | grep emonhub"
+sudo ps -ef | grep emonhub
