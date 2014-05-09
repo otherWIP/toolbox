bf0e1ef9b0269ffe657db2040963f70deea84095
Paul <pb66@users.noreply.github.com>
2014-05-09 12:00:40 +0100

    Create daemon_test.sh

---
+++ daemon_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Executing 'sudo service emonhub stop' command"
+sudo service emonhub stop
+echo "Executing 'sudo service emonhub start' command"
+sudo service emonhub start
+echo "Executing 'sudo service emonhub restart' command"
+sudo service emonhub restart
+echo "Executing 'sudo service emonhub stop' command"
+sudo service emonhub stop
