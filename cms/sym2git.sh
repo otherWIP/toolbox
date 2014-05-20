d4c0e44eb08cfecb83a675e118c6dd6788c1e6ac
Paul <pb66@users.noreply.github.com>
2014-05-20 11:44:06 +0100

    Update sym2git.sh

--- cms/sym2git.sh
+++ cms/sym2git.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
+### set location to install emonCMS files etc
+INST_PATH=/usr/share/emoncms
+
+### set git cloned location
+GIT_PATH=/home/pi/emoncms
+
+### set private emoncms.conf location
+CONF_PATH=/home/pi
+
+### set log file loction
+LOG_PATH=/var/log
+
+### clone emonhub
+cd ~/
+git clone https://github.com/pb66/emoncms.git
+#git clone https://github.com/emoncms/emoncms.git
+
+
+
+
 DATA_PATH=/media/data/emondata
 DATA_PATH=/var/lib
