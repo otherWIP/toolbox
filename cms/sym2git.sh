a5e47d334e77f10af3aaf9556427b068d0eb82f3
Paul <pb66@users.noreply.github.com>
2014-05-20 12:50:18 +0100

    Update sym2git.sh

--- cms/sym2git.sh
+++ cms/sym2git.sh
@@ -8,6 +8,9 @@ GIT_PATH=/home/pi/emoncms
 ### set private emoncms.conf location
 CONF_PATH=/home/pi
 
+### set data loction
+DATA_PATH=/data/cms
+
 ### set log file loction
 LOG_PATH=/var/log
 
@@ -20,6 +23,27 @@ git clone https://github.com/pb66/emoncms.git
 sudo rm -r -f $INST_PATH
 sudo mkdir -p $INST_PATH
 sudo ln -s $GIT_PATH $INST_PATH/www
+
+#link
+
+# Create the required feed directories and set ownership
+DATA_PATH=/data/cms
+sudo mkdir -p $DATA_PATH/phpfina
+sudo mkdir -p $DATA_PATH/phpfiwa
+sudo mkdir -p $DATA_PATH/phptimeseries
+sudo mkdir -p $DATA_PATH/timestore
+sudo mkdir -p $DATA_PATH/mysql
+sudo ln -s $DATA_PATH/phpfina /var/lib
+sudo ln -s $DATA_PATH/phpfiwa /var/lib
+sudo ln -s $DATA_PATH/phptimeseries /var/lib
+sudo ln -s $DATA_PATH/timestore /var/lib
+sudo ln -s $DATA_PATH/mysql /var/lib
+sudo chown www-data:root /var/lib/phpfina/
+sudo chown www-data:root /var/lib/phpfiwa/
+sudo chown www-data:root /var/lib/phptimeseries/
+sudo chown www-data:root /var/lib/timestore/
+
+
 
 
 # install dependencies
